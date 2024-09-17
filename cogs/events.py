7400e121068a0328343e3db5fd7ae21c2b23f47e
NickDerry2023 <nickd@caliwebdesignservices.com>
2024-09-17 12:40:40 +0000

    [UPDATE] Created a message notifier that will show when a member joins the guild.

--- cogs/events.py
+++ cogs/events.py
@@ -16,6 +16,14 @@ class MerxEvents(commands.Cog):
         await self.merx.change_presence(activity=discord.Activity(name="mb-help | beta.merxbot.xyz", type=discord.ActivityType.watching))
         print(self.merx.user.name + " is ready.")
             
+    
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member):
+        welcome_channel = discord.utils.get(member.guild.text_channels, name="welcome")
+        if welcome_channel:
+            member_count = member.guild.member_count
+            await welcome_channel.send(f"> {member.mention} Welcome to **{member.guild.name}**! We now have {member_count} members. 🎉 ")            
+
 
 async def setup(merx):
   await merx.add_cog(MerxEvents(merx))
