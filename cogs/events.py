56d66aed198cc6ba87130a69c95e73f2521828cb
feme12 <bobboy3454@gmail.com>
2024-09-29 03:11:10 +0000

    [UPDATE] Made it so if it is in development enviorment it will leave all un-whitelisted guilds

--- cogs/events.py
+++ cogs/events.py
@@ -5,10 +5,14 @@ import json
 import os
 import uuid
 import shortuuid
+from discord import Embed
 from discord.ext import tasks, commands
 from discord.ext.commands.context import Context
 from cogs.utils.embeds import ErrorEmbed, PermissionDeniedEmbed
 from cogs.utils.errors import send_error_embed
+from cogs.utils.constants import MerxConstants
+
+constants = MerxConstants()
 
 
 CHANNEL_NAME_FOR_WELCOME = ["chat", "general"]
@@ -18,6 +22,9 @@ class MerxEvents(commands.Cog):
     def __init__(self, merx):
         self.merx = merx
         self.change_status.start()
+        self.beta_guilds = [
+            1285107028892717118, # Merx Systems Discord
+        ]
         
         
     
@@ -34,7 +41,54 @@ class MerxEvents(commands.Cog):
         ))
 
         print(f"{self.merx.user.name} is ready with {guild_count} guilds and {user_count:,} users.")
+    
 
+    if constants.merx_environment_type() == "Development":
+        @commands.Cog.listener()
+        async def on_ready(self):
+            for guild in self.merx.guilds:
+                id = guild.id
+                owner = guild.get_member(guild.owner_id)
+                is_dev_guild = id in self.beta_guilds
+                channel = self.merx.get_guild(self.beta_guilds[0]).get_channel(1289772928082378852)
+
+                # Check if owner is None
+                if owner is None:
+                    owner_info = "Owner not found"
+                else:
+                    owner_info = f"{owner}({owner.id})"
+
+                embed = Embed(
+                    title="Beta bot added to a guild",
+                    description=f"**NAME:** `{guild.name}`\n**ID:** `{id}`\n**OWNER:** `{owner_info}`\n**IS_DEV_GUILD:** `{is_dev_guild}`"
+                )
+
+                if not is_dev_guild:
+                    await guild.leave()
+                    await channel.send(embed=embed)
+                    
+        @commands.Cog.listener()
+        async def on_guild_join(self, guild: discord.Guild):
+            id = guild.id
+            owner = guild.get_member(guild.owner_id)
+            is_dev_guild = id in self.beta_guilds
+            channel = self.merx.get_guild(self.beta_guilds[0]).get_channel(1289772928082378852)
+
+            # Check if owner is None
+            if owner is None:
+                owner_info = "Owner not found"
+            else:
+                owner_info = f"{owner}({owner.id})"
+
+            embed = Embed(
+                title="Beta bot added to a guild",
+                description=f"**NAME:** `{guild.name}`\n**ID:** `{id}`\n**OWNER:** `{owner_info}`\n**IS_DEV_GUILD:** `{is_dev_guild}`"
+            )
+
+            if not is_dev_guild:
+                await guild.leave()
+            
+            await channel.send(embed=embed)
 
 
     # This updated the guilds and users periodically every 30 seconds.
