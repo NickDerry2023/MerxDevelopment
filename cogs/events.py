08f1be5bcb8de02752a62afb8a18ae6ea241f0e5
feme12 <bobboy3454@gmail.com>
2024-09-29 08:29:11 +0000

    [UPDATE] Made it so everytime you commit the bot you don't have to un comment the on_member_join event in events.py

--- cogs/events.py
+++ cogs/events.py
@@ -105,20 +105,20 @@ class MerxEvents(commands.Cog):
         ))
             
 
-    '''
-    @commands.Cog.listener()
-    async def on_member_join(self, member: discord.Member):
-        welcome_channel = discord.utils.get(member.guild.text_channels, name=CHANNEL_NAME_FOR_WELCOME)
-        welcome_channel = None
-        for channel_name in CHANNEL_NAME_FOR_WELCOME:
-            welcome_channel = discord.utils.get(member.guild.text_channels, name=channel_name)
+    if constants.merx_environment_type() == "Production":
+        @commands.Cog.listener()
+        async def on_member_join(self, member: discord.Member):
+            welcome_channel = discord.utils.get(member.guild.text_channels, name=CHANNEL_NAME_FOR_WELCOME)
+            welcome_channel = None
+            for channel_name in CHANNEL_NAME_FOR_WELCOME:
+                welcome_channel = discord.utils.get(member.guild.text_channels, name=channel_name)
+                if welcome_channel:
+                    break
+            
             if welcome_channel:
-                break
-        
-        if welcome_channel:
-            member_count = member.guild.member_count
-            await welcome_channel.send(f"{member.mention} Welcome to **{member.guild.name}**! Feel free to explore. We now have **{member_count}** members. 🎉")         
-    '''
+                member_count = member.guild.member_count
+                await welcome_channel.send(f"{member.mention} Welcome to **{member.guild.name}**! Feel free to explore. We now have **{member_count}** members. 🎉")         
+    
 
 
     # This handles the permission denied and error embeds. It also generates
